1f51a7d81a4a6eb5a44c91c7e395f4f1ccfa73e7
Ven <0ventox0@gmail.com>
2021-11-30 15:04:49 +0100

    Fixed bugs

--- src/ad.ts
+++ src/ad.ts
@@ -1,4 +1,7 @@
 import ActiveDirectory from "activedirectory2";
+import bcrypt from "bcrypt";
+
+import {Sessions} from "./session";
 
 const ad = new ActiveDirectory({
     url: "ldap:***REMOVED***",
@@ -7,20 +10,33 @@ const ad = new ActiveDirectory({
     password: "***REMOVED***",
 });
 
+interface err{
+    lde_message: string;
+}
+
+
+
+export const generateSessionID = (): string => bcrypt.hashSync(Math.random().toString(), 10);
 
 
 
 //make a function which will return a callback
 export function checkUser(username: string, password: string, callback: (user: boolean) => void) {
-    ad.authenticate(username, password, (err, user) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        if (!user) {
-            console.error("User not found");
-            return;
-        }
-        callback(user);
+    return new Promise((resolve, reject) =>  {
+        //@ts-ignore
+        ad.authenticate(username, password, (err:err , user) => {
+            if (err) {
+                console.log(err);
+                console.log(typeof err)
+
+                if(err.lde_message.includes("data 52e")) reject("Wrong Username or password!");
+                else reject(err); 
+            }
+            if (!user) {
+                console.error("User not found");
+                reject("User not found");
+            }        
+            callback(user);
+        });
     });
 }
