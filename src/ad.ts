fa60fdc2792588a9e2389e125013a0b3d559734c
Ven <0ventox0@gmail.com>
2021-11-30 15:08:43 +0100

    .env für Sicherheit

--- src/ad.ts
+++ src/ad.ts
@@ -6,8 +6,8 @@ import {Sessions} from "./session";
 const ad = new ActiveDirectory({
     url: "ldap://azrweudc01.jumbo.net",
     baseDN: "dc=jumbo,dc=net",
-    username: "PMWNTMAIl\\***REMOVED***",
-    password: "***REMOVED***",
+    username: process.env.AZURE_USER as string,
+    password: process.env.AZURE_PASSWD as string,
 });
 
 interface err{
