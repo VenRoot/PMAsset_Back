edbf6747a38e009629cc1a17dce96179ca6ae393
VenRoot <0ventox0@gmail.com>
2022-01-19 11:34:41 +0100

    Monitor-Handling hinzugefügt

--- src/logic.ts
+++ src/logic.ts
@@ -1,5 +1,6 @@
+import { Response } from "express";
 import { Item } from "./interface";
-import {addEntry, deleteEntry, editEntry} from "./sql";
+import {addEntry, deleteEntry, editEntry, query} from "./sql";
 
 export const setData = async (request: Item, method: "PUT" | "POST" | "DELETE") => {
     switch(method)
@@ -11,4 +12,25 @@ export const setData = async (request: Item, method: "PUT" | "POST" | "DELETE") 
         case "DELETE":
             await deleteEntry(request); break;
     }
+};
+
+
+export const SetMonitors = async (ITMon: string[], ITPC: string, res: Response, autoAktiv: boolean) =>
+{
+    const result = await query(`UPDATE dbo.PC SET EQUIPMENT = '${JSON.stringify(ITMon)}' WHERE [ITNR] = '${ITPC}'`).catch(err => {
+        console.error(err);
+        res.status(500).send(JSON.stringify(err));
+    });
+    if(!result) {res.status(404).end(JSON.stringify({message: "No entries found", status: 404})); return null}
+
+    if(autoAktiv)
+    {
+        const result = await query(`UPDATE dbo.PC SET STATUS = 'Aktiv' WHERE [ITNR] = '${ITPC}'`).catch(err => {
+            console.error(err);
+            res.status(500).send(JSON.stringify(err));
+        });
+        if(!result) {res.status(404).end(JSON.stringify({message: "No entries found", status: 404})); return null}
+    }
+
+    res.status(200).end(JSON.stringify({message: "Entries changed", status: 200}));
 };
