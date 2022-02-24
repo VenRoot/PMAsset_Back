13e66b34bd4cbfd78827ec01521c8a2b40374361
VenRoot <0ventox0@gmail.com>
2022-02-24 16:10:59 +0100

    Dev-Database

--- src/sql.ts
+++ src/sql.ts
@@ -15,7 +15,7 @@ const config: sql.config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: "***REMOVED***.***REMOVED***.net",
-    database: process.env.DB_DATABASE,
+    database: process.env.DEV ? process.env.DEV_DB_DATABASE : process.env.DB_DATABASE,
     port: parseInt(process.env.DB_PORT || "1433"),
     options: {
         encrypt: true,
